d136a815eec3ab26d70fbcf43d35b1cbce7cce40
Eduardo Krebs <krsaedan@gmail.com>
2022-12-16 09:38:19 -0300

    test

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
+import Request
 import uvicorn
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root(request: Request):
+    print('request.headers')
+    print(request.headers)
+    print('request.headers[Authorization]')
+    print(request.headers['Authorization'])
     return {"message": "Hello World"}
 
-if __name__ == '__main__':
-    uvicorn.run(app, port=8080, host='0.0.0.0')
+# if __name__ == '__main__':
+#     uvicorn.run(app, port=8080, host='0.0.0.0')
